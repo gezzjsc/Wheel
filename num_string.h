7bd205292a438d4c2de8e4fd53cb5450183ce9eb
gezhengzheng <gezzjsc@163.com>
2017-07-31 22:14:34 +0800

    Update num_string.h

--- num_string.h
+++ num_string.h
@@ -3,7 +3,6 @@
 #include <string>
 #include <sstream>
 
-//demo:	StringToNum<int>(str1, i1)
 template <typename Type> 
 bool StringToNum(const std::string& str, Type &num) {
 	std::istringstream iss(str);
@@ -12,7 +11,6 @@ bool StringToNum(const std::string& str, Type &num) {
 	return true;
 }
 
-//demo:	NumToString<int>(i1, str4)
 template <typename Type> 
 bool NumToString(const Type& num, std::string &str) {
 	std::ostringstream oss;
