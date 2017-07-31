bc3e026dd56eb8f7c03fad3062a87f770c040401
gezhengzheng <gezzjsc@163.com>
2017-07-31 15:55:22 +0800

    add number and string converter

---
+++ num_string.h
@@ -0,0 +1,21 @@
+﻿#include <string>
+#include <sstream>
+
+//demo:	StringToNum<int>(str1, i1)
+template <typename Type> 
+bool StringToNum(const std::string& str, Type &num) {
+	std::istringstream iss(str);
+	if (!(iss >> num))
+		return false;
+	return true;
+}
+
+//demo:	NumToString<int>(i1, str4)
+template <typename Type> 
+bool NumToString(const Type& num, std::string &str) {
+	std::ostringstream oss;
+	if (!(oss << num))
+		return false;
+	str = oss.str();
+	return true;
+}
